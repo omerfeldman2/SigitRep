12255c0791e9143902c6d5eb46ff8e7fa63ad626
Omer Feldman <omerfeldman1234@gmail.com>
2019-02-16 21:44:34 +0200

    Exrecise 5 added: Id Validation check

--- Sigit.py
+++ Sigit.py
@@ -107,6 +107,28 @@ def ContractionString(st):
     return "" + str(let) + "" + str(count) + "" + str(ContractionString(st[count:]))
 
 
+# Exrecise 5:
+# ID validation check Exrecise:
+def ID_Validation(id):
+    if len(id) != 9:
+        return False
+    if id.isdigit() is False:
+        return False
+    sum = 0
+    num1 = 0
+    num2 = 0
+    for i in range(0,8,2):
+        num1 = int(id[i]) * 1
+        num2 = int(id[i+1]) * 2
+        if num2 > 9:
+            num2 = int(num2 / 10 + num2 % 10)
+        sum += num1 + num2
+    num = (int(sum/10) + 1)*10 - sum
+    if num == int(id[8]):
+        return True
+    return False
+
+
 def main():
     # Main function:
     # checks the pass code and the TriesLeft
@@ -130,11 +152,16 @@ def main():
            [1,0,2],
            [1,2,0]]
     TicTacToe(Mat)
-    """
+
+    # Exrecise 4 Example:
     st = "aabbbbcdddeaaaaa"
     st = ContractionString(st)
     print(st)
+    """
 
+    #Exrecise 5 Example:
+    id = '322536319'
+    print(ID_Validation(id))
 
 if __name__ == '__main__':
     main()
