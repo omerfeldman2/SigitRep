248b6840979974280c63ff89702db487d1cf087d
Omer Feldman <omerfeldman1234@gmail.com>
2019-02-16 23:38:17 +0200

    Exrecise 7 added: Decorator Cache Exrecise

--- Sigit.py
+++ Sigit.py
@@ -2,6 +2,7 @@
 PassCode = '1234'
 Balance = 50000
 TriesLeft = 3
+Cache = {}
 
 # Exrecise 1:
 def ATM_Interface():
@@ -137,6 +138,16 @@ def Map(Func, List):
         newList.append(Func(obj))
     return newList
 
+
+# Exrecise 7:
+# Decorator Cache Exrecise:
+def Decorator_Cache(ls):  # ls represent the input can be to the function:
+    CacheFunction = SumOfList  # For example SumOfList is our heavy function
+    global Cache
+    if (str(ls) in Cache) == False:
+        Cache[str(ls)] = CacheFunction(ls)
+    return Cache[str(ls)]
+
 def main():
     # Main function:
     # checks the pass code and the TriesLeft
@@ -170,13 +181,24 @@ def main():
     #Exrecise 5 Example:
     id = '322536319'
     print(ID_Validation(id))
-    """
+
 
     #Exrecise 6 Example:
     ls = ["322536319", "12345", "207301949"]
     newList = Map(ID_Validation, ls)
     for obj in newList:
         print(obj)
+    """
+
+    # Exrecise 7 Example:
+    ls1 = [1, 2, 3, 4, 5]
+    ls2 = [2, 4, 6, 8, 10]
+    ls3 = [1, 2, 3, 4, 5]
+    print(Decorator_Cache(ls1))
+    print(Decorator_Cache(ls2))
+    print(Decorator_Cache(ls3))
+
+
 
 
 if __name__ == '__main__':
